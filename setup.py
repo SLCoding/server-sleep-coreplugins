ce4767da8bbaab331967ff78deceb534c0a2581f
schuette <schuette.marcus@googlemail.com>
2017-02-27 23:34:48 +0100

    Added base structure

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='server-sleep-coreplugins',
+      version='0.1',
+      description='These are the base plugins for server-sleep.',
+      url='https://github.com/SLCoding/server-sleep-coreplugins',
+      author='SourceLan',
+      author_email='schuette.marcus@googlemail.com',
+      license='MIT',
+      packages=['server_sleep_coreplugins'],
+      install_requires=[
+          'server-sleep-api',
+          'configparser'
+      ],
+      zip_safe=False)
